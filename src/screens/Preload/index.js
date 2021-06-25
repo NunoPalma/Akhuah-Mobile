8ef390d7023336808eecde58198e316dab45eeef
NuNoPalma <nunopalma98@gmail.com>
2021-06-25 01:47:10 +0100

    Added preload screen animations

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,32 +1,54 @@
-import React, { useEffect } from 'react';
-import { Container, LoadingWheel } from '../../../core_styles';
-import { StyleSheet, Text, Image, AsyncStorage } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Container} from '../../../core_styles';
+import {AsyncStorage, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import LottieView from "lottie-react-native";
 
 export default () => {
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  useEffect(() => {
-    const verifyAuthentication = async() => {
-      const authenticationToken = await AsyncStorage.getItem('authentication_token');
+    useEffect(() => {
+        const verifyAuthentication = async () => {
+            const authenticationToken = await AsyncStorage.getItem('authentication_token');
 
-      if (authenticationToken != null) {
-        console.log("token exists");
-        // validate token
-      } else {
-            console.log("sign in");
+            if (authenticationToken == null) {
+                console.log("token exists");
+                // validate token
+            } else {
+                console.log("sign in");
 
-          navigation.navigate('SignIn');
-      }
-    }
+                navigation.navigate('SignIn');
+            }
+        }
 
-    verifyAuthentication();
-  });
+        verifyAuthentication();
+    });
 
-  return(
-    <Container>
-      <LoadingWheel size="large" color="#FFFFF"/>
-    </Container>
-  );
+    return (
+        <Container>
+            <LottieView
+                autoPlay={true}
+                autoSize={true}
+                style={{
+                    flex: 1,
+                    width: '100%',
+                    bottom: -90
+                }}
+                source={require('../../../assets/fish_tank.json')}
+            />
+            <LottieView
+                autoPlay={true}
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'absolute',
+                    height: '50%',
+                    bottom: 150
+                }}
+                source={require('../../../assets/loading2.json')}
+            />
+
+        </Container>
+    );
 };
